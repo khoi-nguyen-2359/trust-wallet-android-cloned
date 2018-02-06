4d9dfbc978795ad0be1628307cc7032db0488d01
msubkhankulov <m.subkhankulov@gmail.com>
2018-02-06 22:21:59 +0000

    Fix QR unit tests

--- app/src/test/java/com/wallet/crypto/trustapp/QRExtractorTest.java
+++ app/src/test/java/com/wallet/crypto/trustapp/QRExtractorTest.java
@@ -1,102 +1,121 @@
 package com.wallet.crypto.trustapp;
 
-import com.wallet.crypto.trustapp.util.QRURLParser;
+import android.text.TextUtils;
 
+import com.wallet.crypto.trustapp.util.QRUri;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(TextUtils.class)
 public class QRExtractorTest {
+
+    @Before
+    public void setup() {
+        PowerMockito.mockStatic(TextUtils.class);
+        PowerMockito.when(TextUtils.isEmpty(any(CharSequence.class))).thenAnswer(new Answer<Boolean>() {
+            @Override
+            public Boolean answer(InvocationOnMock invocation) throws Throwable {
+                CharSequence a = (CharSequence) invocation.getArguments()[0];
+                return !(a != null && a.length() > 0);
+            }
+        });
+    }
 
     @Test
     public void extractingIsCorrect() throws Exception {
 
-        QRURLParser parser = QRURLParser.getInstance();
-
         // Correct string
-        String extractedString = parser.extractAddressFromQrString("ethereum:0x0000000000000000000000000000000000000000?value=0");
+        String extractedString = QRUri.parse("ethereum:0x0000000000000000000000000000000000000000?value=0").getAddress();
         assertTrue("0x0000000000000000000000000000000000000000".equals(extractedString));
 
         // Protocol with spaces
-        extractedString = parser.extractAddressFromQrString("NG node:0x0000000000000000000000000000000000000000?value=0");
+        extractedString = QRUri.parse("NG node:0x0000000000000000000000000000000000000000?value=0").getAddress();
         assertTrue("0x0000000000000000000000000000000000000000".equals(extractedString));
 
         // Protocol with upper case
-        extractedString = parser.extractAddressFromQrString("PROTOCOL:0x0000000000000000000000000000000000000000?value=0");
+        extractedString = QRUri.parse("PROTOCOL:0x0000000000000000000000000000000000000000?value=0").getAddress();
         assertTrue("0x0000000000000000000000000000000000000000".equals(extractedString));
 
         // String without value
-        extractedString = parser.extractAddressFromQrString("ethereum:0x0000000000000000000000000000000000000000");
+        extractedString = QRUri.parse("ethereum:0x0000000000000000000000000000000000000000").getAddress();
         assertTrue("0x0000000000000000000000000000000000000000".equals(extractedString));
 
         // Lowed case
-        extractedString = parser.extractAddressFromQrString("ethereum:0xabcdef0000000000000000000000000000000000");
+        extractedString = QRUri.parse("ethereum:0xabcdef0000000000000000000000000000000000").getAddress();
         assertTrue("0xabcdef0000000000000000000000000000000000".equals(extractedString));
 
         // Upper case
-        extractedString = parser.extractAddressFromQrString("ethereum:0xABC0000000000000000000000000000000000000");
+        extractedString = QRUri.parse("ethereum:0xABC0000000000000000000000000000000000000").getAddress();
         assertTrue("0xabc0000000000000000000000000000000000000".equals(extractedString));
 
         // Mixed case
-        extractedString = parser.extractAddressFromQrString("ethereum:0xABCdef0000000000000000000000000000000000");
+        extractedString = QRUri.parse("ethereum:0xABCdef0000000000000000000000000000000000").getAddress();
         assertTrue("0xabcdef0000000000000000000000000000000000".equals(extractedString));
 
         // Address without value
-        extractedString = parser.extractAddressFromQrString("0x0000000000000000000000000000000000000000");
+        extractedString = QRUri.parse("0x0000000000000000000000000000000000000000").getAddress();
         assertTrue("0x0000000000000000000000000000000000000000".equals(extractedString));
 
         // Address with value
-        extractedString = parser.extractAddressFromQrString("0x0000000000000000000000000000000000000000?value=0");
+        extractedString = QRUri.parse("0x0000000000000000000000000000000000000000?value=0").getAddress();
         assertTrue("0x0000000000000000000000000000000000000000".equals(extractedString));
 
         // Address with a different protocol
-        extractedString = parser.extractAddressFromQrString("OMG:0x0000000000000000000000000000000000000000");
+        extractedString = QRUri.parse("OMG:0x0000000000000000000000000000000000000000").getAddress();
         assertTrue("0x0000000000000000000000000000000000000000".equals(extractedString));
 
         // Address longer than expected, parse out an address anyway
-        extractedString = parser.extractAddressFromQrString("0x0000000000000000000000000000000000000000123");
+        extractedString = QRUri.parse("0x0000000000000000000000000000000000000000123").getAddress();
         assertTrue("0x0000000000000000000000000000000000000000".equals(extractedString));
 
         // Two parameters
-        extractedString = parser.extractAddressFromQrString("notethereum:0x0000000000000000000000000000000000000abc?value=0&symbol=USD");
+        extractedString = QRUri.parse("notethereum:0x0000000000000000000000000000000000000abc?value=0&symbol=USD").getAddress();
         assertTrue("0x0000000000000000000000000000000000000abc".equals(extractedString));
 
         // Parse out address even with protocol missing
-        extractedString = parser.extractAddressFromQrString(":0x0000000000000000000000000000000000000abc?value=0invalid");
+        extractedString = QRUri.parse(":0x0000000000000000000000000000000000000abc?value=0invalid").getAddress();
         assertTrue("0x0000000000000000000000000000000000000abc".equals(extractedString));
 
         // Two query parameters
-        extractedString = parser.extractAddressFromQrString("ethereum:0x0000000000000000000000000000000000000123?key=value&key=value");
+        extractedString = QRUri.parse("ethereum:0x0000000000000000000000000000000000000123?key=value&key=value").getAddress();
         assertTrue("0x0000000000000000000000000000000000000123".equals(extractedString));
 
         // Ampersand on the end
-        extractedString = parser.extractAddressFromQrString("ethereum:0x0000000000000000000000000000000000000123?key=value&key=value&");
+        extractedString = QRUri.parse("ethereum:0x0000000000000000000000000000000000000123?key=value&key=value&").getAddress();
         assertTrue("0x0000000000000000000000000000000000000123".equals(extractedString));
 
         // Parse out non-hex characters
-        extractedString = parser.extractAddressFromQrString("ethereum:0x0000000000000000000000000000000000000XyZ?value=0invalid");
+        extractedString = QRUri.parse("ethereum:0x0000000000000000000000000000000000000XyZ?value=0invalid").getAddress();
         assertTrue("0x0000000000000000000000000000000000000xyz".equals(extractedString));
 
         // Negative: null when address too short
-        extractedString = parser.extractAddressFromQrString("ethereum:0x0000000000000000abc?value=0invalid");
-        assertTrue(extractedString == null);
+        assertTrue(QRUri.parse("ethereum:0x0000000000000000abc?value=0invalid") == null);
     }
 
     @Test
     public void parseQRURLTest() {
-        QRURLParser parser = QRURLParser.getInstance();
-        QRURLParser.QrUrlResult result;
         Map<String, String> params;
 
-        result = parser.parse("protocol:0x0000000000000000000000000000000000000XyZ?k1=v1");
+        QRUri result = QRUri.parse("protocol:0x0000000000000000000000000000000000000XyZ?k1=v1");
         assertTrue("protocol".equals(result.getProtocol()));
         assertTrue("0x0000000000000000000000000000000000000xyz".equals(result.getAddress()));
 
@@ -105,7 +124,7 @@ public class QRExtractorTest {
         assertTrue(params.equals(result.getParameters()));
 
         // No parameters
-        result = parser.parse("protocol:0x0000000000000000000000000000000000000XyZ");
+        result = QRUri.parse("protocol:0x0000000000000000000000000000000000000XyZ");
         assertTrue("protocol".equals(result.getProtocol()));
         assertTrue("0x0000000000000000000000000000000000000xyz".equals(result.getAddress()));
 
@@ -113,7 +132,7 @@ public class QRExtractorTest {
         assertTrue(params.equals(result.getParameters()));
 
         // No parameters
-        result = parser.parse("protocol:0x0000000000000000000000000000000000000XyZ?");
+        result = QRUri.parse("protocol:0x0000000000000000000000000000000000000XyZ?");
         assertTrue("protocol".equals(result.getProtocol()));
         assertTrue("0x0000000000000000000000000000000000000xyz".equals(result.getAddress()));
 
@@ -121,7 +140,7 @@ public class QRExtractorTest {
         assertTrue(params.equals(result.getParameters()));
 
         // Multiple query params
-        result = parser.parse("naga coin:0x0000000000000000000000000000000000000XyZ?k1=v1&k2=v2");
+        result = QRUri.parse("naga coin:0x0000000000000000000000000000000000000XyZ?k1=v1&k2=v2");
         assertTrue("naga coin".equals(result.getProtocol()));
         assertTrue("0x0000000000000000000000000000000000000xyz".equals(result.getAddress()));
 
@@ -131,7 +150,7 @@ public class QRExtractorTest {
         assertTrue(params.equals(result.getParameters()));
 
         // Too many ':'
-        result = parser.parse("something:coin:0x0000000000000000000000000000000000000XyZ?k1=v1&k2=v2");
+        result = QRUri.parse("something:coin:0x0000000000000000000000000000000000000XyZ?k1=v1&k2=v2");
         assertTrue(result == null);
     }
 }
